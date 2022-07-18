caa51b3d264e98b89ce17c79441e00345ec3694f
patrickgatiri <patrickgatiri@gmail.com>
2022-07-18 17:15:53 +0300

    read metabase URL

--- src/metabase-config.py
+++ src/metabase-config.py
@@ -1,21 +1,26 @@
 import os
 import sys
+import requests
 
-# Metabase URL Environment Variable
-metabase_url_env = "METABASE_URL"
+# Environment Variables
+metabase_host_env = "MB_HOST"
+metabase_port_env = "MB_PORT"
+metabase_protocol_env = "MB_PROTOCOL"
 
-def getMetabaseURL():
-    # Default value for the Metabase URL
-    metabase_url = ""
+def build_connection_string(metabase_protocol, metabase_host, metabase_port):
+    return "{}://{}:{}"/format(metabase_protocol, metabase_host, metabase_port)
 
+def getEnvVariable(env_variable_name):
     try:
-        if os.environ[metabase_url_env]:
-            metabase_url = os.environ[metabase_url_env]
+        if os.environ[env_variable_name]:
+            metabase_url = os.environ[env_variable_name]
             print(metabase_url)
     except KeyError:
-        print(metabase_url_env, ' environment variable not set.')
+        print(env_variable_name, ' environment variable not set.')
         sys.exit(1)
     
     return metabase_url
 
-getMetabaseURL()
+metabase_protocol = getEnvVariable(metabase_protocol_env)
+metabase_host = getEnvVariable(metabase_host_env)
+metabase_port = getEnvVariable(metabase_port_env)
